05926e97f13b921cd5230ef02ef122747ebcf6ef
tirawally <donattocancioepet20@gmail.com>
2025-06-15 14:03:52 -0300

    agrego index y css de la vista nivel ganado

--- ESI_VR/src/App.jsx
+++ ESI_VR/src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PerfilUsuario from "./components/views/PerfilUsuario";
-
+import NivelGanado from "/components/views/NivelGanado";
 function App() {
     return (
         <PerfilUsuario/>
     );
 }
+
+function App() {
+    return (
+        <NivelGanado/>
+    );
+}
+
 
 export default App;
