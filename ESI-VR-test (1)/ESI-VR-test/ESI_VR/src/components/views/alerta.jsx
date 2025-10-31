b54ba3245874023a24a7fd3f73063cbafbe21a22
Matias Rambeaud <matiasrambeaud@gmail.com>
2025-10-30 21:45:52 -0300

    borrar basura

--- ESI-VR-test (1)/ESI-VR-test/ESI_VR/src/components/views/alerta.jsx
+++
@@ -1,8 +0,0 @@
-
-const alerta = () => (
-  <div style={{ fontSize: '48px', color: 'orange' }}>
-    ⚠️
-  </div>
-);
-
-export default alerta;
--- ESI-VR-test (1)/ESI-VR-test/ESI_VR/src/listas/leeralumnos.jsx
+++
@@ -1,6 +0,0 @@
-import {collection, getdocs } from "firebase/firestore";
-
-const col = collection(db, "Alumnos");
-const snap = await getdocs(col)
-const data = snap.docs.map (d=> ({ id: d.id, ...d.data()}));
-console.log(data);
